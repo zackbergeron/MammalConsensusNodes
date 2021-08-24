602c6b96176c2be280177f5a0944a4e9677948cf
alexknyshov <alexknyshov@gmail.com>
2021-08-24 15:00:41 -0400

    folder var

--- iqtree/iqtree_prep.sh
+++ iqtree/iqtree_prep.sh
@@ -9,10 +9,10 @@
 script_work_dir="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/"
 scripts_dir="/home/aknyshov/alex_data/tree_shew_analysis/TreeshrewProject/iqtree/"
 aligned_loci_path="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/aligned_loci/"
+array_work_folder="iqtree_assessment_primates"
 cd ${script_work_dir}
-mkdir iqtree_assessment
-cd iqtree_assessment
-mkdir rates
+mkdir ${array_work_folder}
+cd ${array_work_folder}
 mkdir scf
 ls ${aligned_loci_path} | rev | cut -f1 -d/ | rev | split - aligned_loci_list_
 arrayN=$(ls aligned_loci_list_* | wc -l)
