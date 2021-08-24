561e2a5f24bb2be76cfbb273f5850514c1047170
alexknyshov <alexknyshov@gmail.com>
2021-08-24 15:01:56 -0400

    mean and sd TN93 dist added

--- saturation/saturation.R
+++ saturation/saturation.R
@@ -19,12 +19,15 @@ for (i in 1:length(dnafiles)){
 	rawdist <- dist.dna(alignment, model = "raw", pairwise.deletion = T, as.matrix = T)
 	tn93dist <- dist.dna(alignment, model = "TN93", pairwise.deletion = T, as.matrix = T)
 	tn93dist2 <- tn93dist[lower.tri(tn93dist)]
+	# get mean tn93 dist
+	mean_dist <- mean(tn93dist2)
+	sd_dist <- sd(tn93dist2)
 	rawdist2 <- rawdist[lower.tri(rawdist)]
 	regress <- lm(rawdist2 ~ tn93dist2)
 	# get slope
 	slope <- coef(regress)[2]
 	# get r-squared
 	Rsquared <- summary(regress)$r.squared
-	output <- paste(dnafiles[i],slope,Rsquared,sep=",")
+	output <- paste(dnafiles[i],slope,Rsquared,mean_dist,sd_dist,sep=",")
 	write(x=output,file="saturation_output.csv", append=T)
 }
