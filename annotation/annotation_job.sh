46daace3a6e2dd92583867b685b1d05c3f7cd9ae
alexknyshov <alexknyshov@gmail.com>
2021-08-25 11:12:07 -0400

    getTaxContigs added

--- annotation/annotation_job.sh
+++ annotation/annotation_job.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 #SBATCH --job-name="annotate"
-#SBATCH --time=48:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=100:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=20   # processor core(s) per node
 
 cd $SLURM_SUBMIT_DIR
 date
 scripts_dir="/home/aknyshov/alex_data/tree_shew_analysis/TreeshrewProject/annotation/"
-fileToMap="../refs_for_annotation/Homo_sapiens.fas"
+sisrsContigs="../aligned_loci/"
+taxonName="Homo_sapiens"
 assemblyDB="/home/aknyshov/alex_data/tree_shew_analysis/NCBI_genomes/Homo_sapiens/GCF_000001405.39_GRCh38.p13_genomic.mod.fna"
 assemblyGFF="/home/aknyshov/alex_data/tree_shew_analysis/NCBI_genomes/Homo_sapiens/GCF_000001405.39_GRCh38.p13_genomic.gff"
+outputMode="l"
+
+#Andromeda (URI's cluster) specific
+module purge
+module load Biopython/1.78-foss-2020b
+#
+python ${scripts_dir}getTaxContigs.py ${taxonName} ${sisrsContigs}
 
 #Andromeda (URI's cluster) specific
 module purge
@@ -18,7 +26,7 @@ module load BLAST+
 
 makeblastdb -in ${assemblyDB} -dbtype nucl
 
-blastn -query ${fileToMap} -db ${assemblyDB} -outfmt 6 -num_threads 20 > blast_results.blast
+blastn -query ${taxonName}.fasta -db ${assemblyDB} -outfmt 6 -num_threads 20 > blast_results.blast
 
 python3 ${scripts_dir}annotation_blast_parser.py blast_results.blast > full_table.bed
 
@@ -31,6 +39,6 @@ module load BEDTools/2.27.1-foss-2018b
 
 bedtools intersect -a full_table_sorted.bed -b ${assemblyGFF} -wa -wb > full_table_annotated.bed
 
-python3 ${scripts_dir}annotation_bed2table.py full_table_annotated.bed c > annotations.csv
+python3 ${scripts_dir}annotation_bed2table.py full_table_annotated.bed ${outputMode} > annotations.csv
 
 date
