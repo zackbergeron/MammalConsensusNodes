ab0527d51f61e002a193425dea22bc39e654f9d1
alexknyshov <alexknyshov@gmail.com>
2021-09-02 14:28:34 -0400

    add variables

--- amas/run_amas.py
+++ amas/run_amas.py
@@ -5,6 +5,7 @@ import subprocess
 fasta_folder = sys.argv[1]
 total_cores = sys.argv[2]
 files = glob.glob(fasta_folder+"/*.fasta")
+amas = sys.argv[3]
 
 fileList = []
 count = 0
@@ -13,7 +14,7 @@ for f in files:
 	fileList.append(f)
 	count += 1
 	if count == 1000:
-		cmd = "python ~/alex_data/andromeda_tools/AMAS/amas/AMAS.py summary -c "+total_cores+" -o amas_output_temp.txt -f fasta -d dna -i "+" ".join(fileList)
+		cmd = "python "+amas+" summary -c "+total_cores+" -o amas_output_temp.txt -f fasta -d dna -i "+" ".join(fileList)
 		subprocess.call(cmd, shell=True)
 		if first:
 			subprocess.call("sed -e '$a\\' amas_output_temp.txt > amas_total_results.txt", shell=True)
@@ -23,6 +24,6 @@ for f in files:
 		count = 0
 		fileList = []
 if len(fileList) > 0:
-	cmd = "python ~/alex_data/andromeda_tools/AMAS/amas/AMAS.py summary -c "+total_cores+" -o amas_output_temp.txt -f fasta -d dna -i "+" ".join(fileList)
+	cmd = "python "+amas+" summary -c "+total_cores+" -o amas_output_temp.txt -f fasta -d dna -i "+" ".join(fileList)
 	subprocess.call(cmd, shell=True)
 	subprocess.call("sed -e 1d $f amas_output_temp.txt | sed -e '$a\\' >> amas_total_results.txt", shell=True)
--- amas/run_amas.sh
+++ amas/run_amas.sh
@@ -2,19 +2,22 @@
 #SBATCH --job-name="amas"
 #SBATCH --time=24:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=20   # processor core(s) per node
+#SBATCH --ntasks-per-node=12   # processor core(s) per node
 #SBATCH --mail-user="aknyshov@uri.edu"
 #SBATCH --mail-type=END,FAIL
 
 module purge
 module load Python/3.7.4-GCCcore-8.3.0
+path_to_amas="/home/aknyshov/alex_data/andromeda_tools/AMAS/amas/AMAS.py"
+folder_with_loci="aligned_loci/"
+cores_to_use=12
 
 cd $SLURM_SUBMIT_DIR
 date
 
 mkdir amas_assessments
 
-python ~/alex_data/tree_shew_analysis/TreeshrewProject/amas/run_amas.py aligned_loci/ 12
+python ~/alex_data/tree_shew_analysis/TreeshrewProject/amas/run_amas.py ${folder_with_loci} ${cores_to_use} ${path_to_amas}
 
 mv amas_total_results.txt amas_assessments/
 rm amas_output_temp.txt
