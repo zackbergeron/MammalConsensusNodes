9b4ca07d2da2bd38e1722ee82dd8705cc3ac7601
alexknyshov <alexknyshov@gmail.com>
2022-06-29 09:55:03 -0400

    gtree scripts addded

---
+++ iqtree/iqtree_array_gtree.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name="IQarr"
+#SBATCH --time=48:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=6G
+
+
+cd $SLURM_SUBMIT_DIR
+
+date
+
+fileline=$(sed -n ${SLURM_ARRAY_TASK_ID}p ../iqtree_assessment/array_list.txt)
+aligned_loci_path="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing_v2/aligned_loci/"
+iqtree_exe="/home/aknyshov/alex_data/andromeda_tools/iqtree-2.1.2-Linux/bin/iqtree2"
+
+cat ../iqtree_assessment/${fileline} | while read line
+do
+	echo $line
+	Rscript ${scripts_dir}trimTrees.R ${aligned_loci_path}/${line} ${trees_to_eval} ./trees_${line}.tre
+	${iqtree_exe} -nt 1 -s ${aligned_loci_path}/${line} -pre inference_${line} -alrt 1000 -m GTR+G
+	rm inference_${line}.ckp.gz inference_${line}.iqtree inference_${line}.log inference_${line}.bionj inference_${line}.mldist inference_${line}.uniqueseq.phy
+done
