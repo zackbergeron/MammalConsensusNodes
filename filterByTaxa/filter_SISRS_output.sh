7fa997af33bfb0f6048e923b71e740eff22b6f26
Leann M Biancani <leann.biancani@gmail.com>
2023-03-22 18:56:23 -0400

    fix filter_SISRS_output.py

--- filterByTaxa/filter_SISRS_output.sh
+++ filterByTaxa/filter_SISRS_output.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 #SBATCH --job-name="filter"
-#SBATCH --time=24:00:00  # walltime limit (HH:MM:SS)
-
+#SBATCH --time=30:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # processor core(s) per node - just use 1 
+#SBATCH --mail-user="biancani@uri.edu" #CHANGE to your email
+#SBATCH --mail-type=ALL
 ### adjust/add sbatch flags as needed
 
 cd $SLURM_SUBMIT_DIR
@@ -11,6 +14,13 @@ module purge
 #for URI's Andromeda cluster
 module load Biopython/1.78-foss-2020b 
 
+OUTPUT=/data/schwartzlab/Biancani/PlacentalPolytomy/output/SISRS_out_filtered # change to name and path for output folder (will be created by script!)
+TXNGROUPS=/data/schwartzlab/Biancani/PlacentalPolytomy/filterByTaxa/groups.csv # path to taxon group table (csv)
+LOCI=/data/schwartzlab/Biancani/PLACENTAL/SISRS_out/SISRS_Run/aligned_contigs # path to SISRS loci (aligned contigs)folder
+SEQCOMPLETE=0.33 # taxon sequence completeness, (e.g. 0.33 is 33% non N)
+MINTAXA=18 # minimum number taxa to be present, e.g. 25
+MINGROUPS=4 # minimum number of groups to be present, e.g. 4
+
 # adjust the command as needed, check the correct path to the python script
-mkdir <path for output folder>
-python <path to>filter_SISRS_output.py <path to taxa group table> <path to SISRS loci folder> <path for output folder> <taxon sequence completeness, e.g. 0.33> <num taxa to be present, e.g. 25> <number of groups to be present, e.g. 6>
+mkdir -p $OUTPUT
+python filter_SISRS_output.py $TXNGROUPS $LOCI $OUTPUT $SEQCOMPLETE $MINTAXA $MINGROUPS
