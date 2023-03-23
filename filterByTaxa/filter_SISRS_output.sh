99b3e74dc9984811844909b62a9d8c8d7d5bcd51
Leann M Biancani <leann.biancani@gmail.com>
2023-03-23 15:05:18 -0400

    clean up slurm submission file

--- filterByTaxa/filter_SISRS_output.sh
+++ filterByTaxa/filter_SISRS_output.sh
@@ -6,20 +6,25 @@
 #SBATCH --ntasks-per-node=1   # processor core(s) per node - just use 1 
 #SBATCH --mail-user="biancani@uri.edu" #CHANGE to your email
 #SBATCH --mail-type=ALL
-### adjust/add sbatch flags as needed
+
+## UPDATE PATHS:
+# output folder path (will be created by script if necessary):
+OUTPUT=/data/schwartzlab/Biancani/PlacentalPolytomy/output/SISRS_out_filtered
+# path to taxon group table (csv):
+TXNGROUPS=/data/schwartzlab/Biancani/PlacentalPolytomy/filterByTaxa/groups.csv
+# path to SISRS loci (aligned contigs) folder:
+LOCI=/data/schwartzlab/Biancani/PLACENTAL/SISRS_out/SISRS_Run/aligned_contigs
+
+## UPDATE parameters:
+SEQCOMPLETE=0.33 # taxon sequence completeness, (e.g. 0.33 is 33% non N)
+MINTAXA=18 # minimum number taxa to be present, e.g. 25
+MINGROUPS=4 # minimum number of groups to be present, e.g. 4
 
 cd $SLURM_SUBMIT_DIR
 
 module purge
 #for URI's Andromeda cluster
 module load Biopython/1.78-foss-2020b 
-
-OUTPUT=/data/schwartzlab/Biancani/PlacentalPolytomy/output/SISRS_out_filtered # change to name and path for output folder (will be created by script!)
-TXNGROUPS=/data/schwartzlab/Biancani/PlacentalPolytomy/filterByTaxa/groups.csv # path to taxon group table (csv)
-LOCI=/data/schwartzlab/Biancani/PLACENTAL/SISRS_out/SISRS_Run/aligned_contigs # path to SISRS loci (aligned contigs)folder
-SEQCOMPLETE=0.33 # taxon sequence completeness, (e.g. 0.33 is 33% non N)
-MINTAXA=18 # minimum number taxa to be present, e.g. 25
-MINGROUPS=4 # minimum number of groups to be present, e.g. 4
 
 # adjust the command as needed, check the correct path to the python script
 mkdir -p $OUTPUT
