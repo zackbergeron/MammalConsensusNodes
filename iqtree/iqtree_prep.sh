d7c4758d4ff4e6f42400cdf56075aa7945a5b3c2
Leann M Biancani <leann.biancani@gmail.com>
2023-03-27 13:07:31 -0400

    Add IQ-TREE prep script updates

--- iqtree/iqtree_prep.sh
+++ iqtree/iqtree_prep.sh
@@ -5,16 +5,33 @@
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
 #SBATCH -c 1
 #SBATCH --mem-per-cpu=6G
+#SBATCH --mail-user="biancani@uri.edu"
+#SBATCH --mail-type=ALL
 
-script_work_dir="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/"
-scripts_dir="/home/aknyshov/alex_data/tree_shew_analysis/TreeshrewProject/iqtree/"
-aligned_loci_path="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/aligned_loci/"
-array_work_folder="iqtree_assessment_primates"
+# UPDATE:
+# path to main output directory:
+script_work_dir=/data/schwartzlab/Biancani/PlacentalPolytomy/output
+# name output folder for iqtree (will be created by script if necessary):
+array_work_folder="iqtree_assessment"
+# location of iqtree scripts:
+scripts_dir=/data/schwartzlab/Biancani/PlacentalPolytomy/iqtree
+# path to FILTERED SISRS loci (aligned contigs) folder:
+aligned_loci_path=/data/schwartzlab/Biancani/PlacentalPolytomy/output/SISRS_out_filtered
+# number of simultaneous tasks for subsequent array jobs:
+array_tasks=40
+
 cd ${script_work_dir}
-mkdir ${array_work_folder}
+mkdir -p ${array_work_folder}
 cd ${array_work_folder}
 mkdir scf
-ls ${aligned_loci_path} | rev | cut -f1 -d/ | rev | split - aligned_loci_list_
+# extract filenames from aligned_loci_path and split into bins of 4000
+ls ${aligned_loci_path} | rev | cut -f1 -d/ | rev | split -l 4000 - aligned_loci_list_
 arrayN=$(ls aligned_loci_list_* | wc -l)
 ls aligned_loci_list_* > array_list.txt
-echo sbatch --array=1-${arrayN}%40 ${scripts_dir}iqtree_array.sh
+if [ $arrayN -lt $array_tasks ]
+    then
+      array_tasks=$arrayN
+fi
+
+#echo sbatch --array=1-${arrayN}%{array_tasks} ${scripts_dir}iqtree_array.sh
+echo \#SBATCH --array=${arrayN}%${array_tasks}
