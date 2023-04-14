84ef6fb08b4b806370377f68f10d45d7ea7c02da
Zack Bergeron <100803460+zackbergeron@users.noreply.github.com>
2023-04-14 19:29:52 -0400

    Update filter_SISRS_output.sh

--- filterByTaxa/filter_SISRS_output.sh
+++ filterByTaxa/filter_SISRS_output.sh
@@ -2,30 +2,19 @@
 #SBATCH --job-name="filter"
 #SBATCH --time=30:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=1   # processor core(s) per node - just use 1 
-#SBATCH --mail-user="biancani@uri.edu" #CHANGE to your email
-#SBATCH --mail-type=ALL
-
-## UPDATE PATHS:
-# path to SISRS loci (aligned contigs) folder:
-LOCI=/data/schwartzlab/Biancani/PLACENTAL/SISRS_out/SISRS_Run/aligned_contigs
-# path to folder containing filterByTaxa scripts:
-SCRIPTS=/data/schwartzlab/Biancani/PlacentalPolytomy/annotation
-# path to taxon group table (csv):
-TXNGROUPS=/data/schwartzlab/Biancani/PlacentalPolytomy/filterByTaxa/groups.csv
-# path to output folder for filtered loci (will be created by script if necessary):
-OUTPUT=/data/schwartzlab/Biancani/PlacentalPolytomy/output/SISRS_out_filtered
-
-## UPDATE parameters:
-SEQCOMPLETE=0.33 # taxon sequence completeness, (e.g. 0.33 is 33% non N)
-MINTAXA=18 # minimum number taxa to be present, e.g. 25
-MINGROUPS=4 # minimum number of groups to be present, e.g. 4
+#SBATCH --ntasks-per-node=36   # processor core(s) per node - just use 1 - it do$
+#SBATCH --mail-user="zbergeron@uri.edu" #CHANGE to your user email address
+#SBATCH --mail-type=END,FAIL
+### adjust/add sbatch flags as needed
 
 cd $SLURM_SUBMIT_DIR
 
 module purge
 #for URI's Andromeda cluster
-module load Biopython/1.78-foss-2020b 
+module load Biopython/1.78-foss-2020b
 
-mkdir -p $OUTPUT
-python $SCRIPTS/filter_SISRS_output.py $TXNGROUPS $LOCI $OUTPUT $SEQCOMPLETE $MINTAXA $MINGROUPS
+# adjust the command as needed, check the correct path to the python script
+mkdir /data/schwartzlab/zbergeron/SISRS_mammals/filteredMammalLoci
+python /data/schwartzlab/zbergeron/TreeshrewProject/filterByTaxa/filter_SISRS_output.py /data/schwartzlab/zbergeron/SISRS_mammals/Mammal_filterbytaxa_table.csv /data/schwartzlab/zbergeron/SISRS_mammals/SISRS_Run/aligned_contigs /data/schwartzlab/zbergeron/SISRS_mammals/filteredMammalLoci 0.33 18 4
+
+
